e2082748e08ffd382387755f508d8d52c1b6d35f
SethEddy <viva.la.seth.eddy@gmail.com>
2018-10-30 11:41:05 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -24,15 +24,15 @@ class ProgramLauncherSkill(MycroftSkill):
         
     def handle_launch_intent(self, message):
         utterance = message.data.get('utterance')
-        #xer = re.compile(r'\blaunch program\b', re.IGNORECASE)
-        #utter = xer.sub('', utterance)
+        xer = re.compile(r'\blaunch program\b', re.IGNORECASE)
+        utter = xer.sub('', utterance)
         #rex = re.compile(r'\b \b', re.IGNORECASE)
         #text = rex.sub('*', utter)
         #prog = fnmatch.filter(os.listdir('/usr/share/applications/'), "*" + text + "*.*")
         #program = "'%s'" % "".join(prog)
         self.speak_dialog("launch")
         #subprocess.call(['gtk-launch', program])
-        subprocess.call(['gtk-launch', utterance])
+        subprocess.call(['gtk-launch', utter])
 
     def stop(self):
         pass
